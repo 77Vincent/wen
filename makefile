2fb174349f3e124b364cae6a05a1143bb6af3342
tianqi.wen <tianqi.wen@fastretailing.com>
2024-11-12 11:13:03 +0900

    algolia search WIP

--- makefile
+++ makefile
@@ -4,4 +4,7 @@ run:
 deploy:
 	hugo deploy --target production && hugo deploy --target production-www
 
+index:
+	npm run index-and-send
+
 .PHONY: run, deploy
---
+++ static/js/search.js
@@ -0,0 +1,8 @@
+(function () {
+    docsearch({
+        appId: "ZX8NQDH1YO",
+        apiKey: "0ebd556587726f78be53db0ea5dc8d99",
+        indexName: "wenstudy",
+        container: "#dosearch",
+    });
+}())
